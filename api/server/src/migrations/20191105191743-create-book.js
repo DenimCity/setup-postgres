bb0c8be7caf466cd91d73adce0b233904c093a82
DenimCity <altidorj1@gmail.com>
2019-11-05 14:22:42 -0500

    updated migration

--- api/server/src/migrations/20191105191743-create-book.js
+++ api/server/src/migrations/20191105191743-create-book.js
@@ -1,33 +1,35 @@
-'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Books', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING
-      },
-      price: {
-        type: Sequelize.STRING
-      },
-      description: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Books');
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('Books', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			title: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			price: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			description: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: (queryInterface) => {
+		return queryInterface.dropTable('Books');
+	}
 };
