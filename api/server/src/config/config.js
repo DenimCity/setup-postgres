000e8c7dbdf4068a05d7f939c9f140085a6f2101
DenimCity <altidorj1@gmail.com>
2019-11-05 14:07:45 -0500

    setting up config

---
+++ api/server/src/config/config.js
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+
+export default {
+	// If using onine database
+	// development: {
+	//   use_env_variable: 'DATABASE_URL'
+	// },
+
+	development: {
+		database: 'books',
+		username: 'postgres',
+		password: null,
+		host: '127.0.0.1',
+		dialect: 'postgres'
+	},
+
+	test: {
+		database: 'book_test',
+		username: 'postgres',
+		password: null,
+		host: '127.0.0.1',
+		dialect: 'postgres'
+	},
+
+	production: {
+		database: process.env.DB_NAME,
+		username: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		host: process.env.DB_HOST,
+		dialect: 'postgres'
+	}
+};
